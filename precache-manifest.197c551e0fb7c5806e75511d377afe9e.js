8d4467b084fc1ab3f242bb564deea5189ab59511
unknown <sinha.aditya244@gmail.com>
2019-07-17 01:48:49 +0530

    Updates

---
+++ precache-manifest.197c551e0fb7c5806e75511d377afe9e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e7ae5794490cf275171ce0da1f39c15",
+    "url": "/expense-mgmt-app/index.html"
+  },
+  {
+    "revision": "e210ccb216af69d83911",
+    "url": "/expense-mgmt-app/static/css/main.a51d3c00.chunk.css"
+  },
+  {
+    "revision": "b8f783844602315bcac7",
+    "url": "/expense-mgmt-app/static/js/2.23575782.chunk.js"
+  },
+  {
+    "revision": "e210ccb216af69d83911",
+    "url": "/expense-mgmt-app/static/js/main.327c55d5.chunk.js"
+  },
+  {
+    "revision": "6e5ff90701ee7a7a7c85",
+    "url": "/expense-mgmt-app/static/js/runtime~main.fd625510.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/expense-mgmt-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
